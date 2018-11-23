81ce61fb13a7fbdaec4e7005b4bdf8469e6debcc
Richard Anthony <reanthony@crimson.ua.edu>
2018-11-23 11:19:52 -0600

    Update 11/23

--- src:/member.java
+++ src:/member.java
@@ -5,5 +5,24 @@ import java.util.*;
 
 
 public Member extends User {
+	public String status;
 	
+}
+
+public void setMemberStatus(String newStatus) {
+	status = newStatus;
+}
+
+public void geMemberStatus() {
+	return status;
+}
+
+public void printMemberAttributes() {
+	System.out.println("Member Name: "+ getMemberName());
+	System.out.println("Member Number: " + getMemberNumber());
+	System.out.println("Member Street Address: " + getMemberStreetAddress());
+	System.out.println("City: " + getMemberCity());
+	System.out.println("State: " + getMemberState());
+	System.out.println("Zip Code: " + getMemberZip());
+}
 }
